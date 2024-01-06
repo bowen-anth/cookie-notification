52b094b467f3bc6689223da5ca15234fd8f64ae3
bowen-anth <bowen.anth@gmail.com>
2024-01-06 00:07:16 -0700

    finished

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log("test");
+const CLOSE_BUTTON = document.querySelector(".close-button");
+const TOAST = document.querySelector(".toast");
+
+CLOSE_BUTTON.addEventListener("click", () => {
+  TOAST.style.display = "none";
+  console.log("clicked");
+});
