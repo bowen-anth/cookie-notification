a81f5252231577a7a65d4217a46d3166c67c7bae
bowen-anth <bowen.anth@gmail.com>
2024-01-06 00:14:01 -0700

    added card close button

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const CLOSE_BUTTON = document.querySelector(".close-button");
 const TOAST = document.querySelector(".toast");
+const CARD = document.querySelector(".card");
+const CARD_CLOSE_BUTTON = document.querySelector("#card-close-button");
 
 CLOSE_BUTTON.addEventListener("click", () => {
   TOAST.style.display = "none";
-  console.log("clicked");
+});
+
+CARD_CLOSE_BUTTON.addEventListener("click", () => {
+  CARD.style.display = "none";
 });
